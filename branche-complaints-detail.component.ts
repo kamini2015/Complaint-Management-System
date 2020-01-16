aea49c3774f31002599790d101c2cf25192fee4b
kamini2015 <kamini.chaudhary2015@gmail.com>
2020-01-16 17:10:50 +0530

    Add files via upload

---
+++ branche-complaints-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-branche-complaints-detail',
+  templateUrl: './branche-complaints-detail.component.html'
+})
+export class BrancheComplaintsDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
