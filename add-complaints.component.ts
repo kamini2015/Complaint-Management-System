e28cea9d396ae56d0f0ede928d9738341af881a1
kamini2015 <kamini.chaudhary2015@gmail.com>
2020-01-16 17:13:04 +0530

    Add files via upload

---
+++ add-complaints.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-complaints',
+  templateUrl: './add-complaints.component.html'
+})
+export class AddComplaintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
