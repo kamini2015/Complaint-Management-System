6ad0dd80b8011279c6b4825c0181d1a30f3298b9
kamini2015 <kamini.chaudhary2015@gmail.com>
2020-01-16 17:11:36 +0530

    Add files via upload

---
+++ branches-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-branches-list',
+  templateUrl: './branches-list.component.html'
+})
+export class BranchesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
